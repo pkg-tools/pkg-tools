61b738073916a41c8122f9fe59614917f03d4f66
Joe McKenney <joe@switchboard.cc>
2023-12-28 16:20:49 -0800

    fix(@libs/node-typescript-cli): remove unneeded config + formatting

--- examples/@libs/node-typescript-cli/build.config.ts
+++ examples/@libs/node-typescript-cli/build.config.ts
@@ -1,5 +1,5 @@
-import { config } from '@pkg-tools/build';
+import { config } from "@pkg-tools/build";
 
 export default config.node({
-  entries: ['src/index', 'src/cli'],
+  entries: ["src/index", "src/cli"],
 });
