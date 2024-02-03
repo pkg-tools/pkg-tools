3083c98b5b3c1e378da8aa7dc6c2f9d8151b212a
Joe McKenney <joe@switchboard.cc>
2024-02-03 14:51:22 -0800

    refactor(@pkg-tools/lint): uses @pkg-tools/utilities instead of @pkg-tools/config

---
+++ packages/@pkg-tools/lint/src/config.ts
@@ -0,0 +1,22 @@
+import defu from 'defu';
+import { resolvePkgToolsConfig } from '@pkg-tools/utilities';
+
+import { type ESLint } from 'eslint';
+
+export interface Config extends ESLint.ConfigData {}
+
+export const defaults: Config = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  root: true,
+};
+
+export function getConfig(config: Partial<Config>): Config {
+  return defu(config, resolvePkgToolsConfig()['clean'], defaults);
+}
