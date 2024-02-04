f776708360d418382a1ff6ec8f8b2a34a79d4db2
Joe McKenney <joe@switchboard.cc>
2024-02-03 19:00:02 -0800

    fix(@pkg-tools/config): spelling mistake

--- packages/@pkg-tools/config/src/index.ts
+++ packages/@pkg-tools/config/src/index.ts
@@ -3,13 +3,13 @@ import { type Config as CleanConfig } from '@pkg-tools/clean/config';
 import { type Config as FormatConfig } from '@pkg-tools/format/config';
 import { type Config as LintConfig } from '@pkg-tools/lint/config';
 
-export interface PkgToolsConfig {
+export interface Config {
   build?: BuildConfig;
   clean?: CleanConfig;
   format?: FormatConfig;
   lint?: LintConfig;
 }
 
-export function definePkgToolsConfig(config: PkgToolsConfig): PkgToolsConfig {
+export function defineConfig(config: Config): Config {
   return config;
 }
