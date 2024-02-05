19671f26fa076c39235ef87af6bc84e3a3b1d6be
Joe McKenney <joe@switchboard.cc>
2024-02-04 16:11:00 -0800

    chore: remove @pkg-tools/pkg-tools + update README

--- packages/@pkg-tools/pkg-tools/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * as config from '@pkg-tools/config';
-export * as build from '@pkg-tools/build';
-export * as clean from '@pkg-tools/clean';
-export * as lint from '@pkg-tools/lint';
-export * as format from '@pkg-tools/format';
