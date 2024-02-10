80fa6bd69d91127c5550f91b8cd03b09b7a8bf0b
Joe McKenney <joe@switchboard.cc>
2024-02-09 17:34:06 -0800

    feat: add postinstall scripts to the @pkg-tools/(lint|format)

---
+++ packages/@pkg-tools/lint/postinstall.mjs
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import consola from 'consola';
+import fs from 'node:fs';
+import path from 'node:path';
+
+if (!fs.existsSync(path.resolve(process.env.INIT_CWD, './.eslintrc.cjs'))) {
+  fs.writeFileSync(
+    path.resolve(process.env.INIT_CWD, './.eslintrc.cjs'),
+    `const { getConfig } = require("@pkg-tools/lint/config");
+module.exports = {
+  ...getConfig(),
+};
+`
+  );
+} else {
+  consola.info(
+    `Looks like you already have a .eslintrc.cjs. Not overwriting it.`
+  );
+  consola.info(
+    `For @pkg-tools/lint to work you'll need to create this file manually`
+  );
+  consola.info('It should like like this');
+  console.log(`const { getConfig } = require("@pkg-tools/lint/config");
+module.exports = {
+  ...getConfig(),
+};`);
+}
