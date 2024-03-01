c1b4bd4bca6e4027b901353a102151b68acc8dd4
Joe McKenney <joe@switchboard.cc>
2024-03-01 13:44:39 -0800

    chore(@pkg-tools/create-pkg): checkpoint basic templates

---
+++ create-pkg/templates/node/src/index.ts
@@ -0,0 +1,7 @@
+import consola from 'consola';
+
+const joe = '2';
+
+export function run() {
+  consola.log('running');
+}
---
+++ create-pkg/templates/react/.eslintrc.cjs
@@ -0,0 +1,4 @@
+const { getConfig } = require("@pkg-tools/lint/config");
+module.exports = {
+  ...getConfig(),
+};
